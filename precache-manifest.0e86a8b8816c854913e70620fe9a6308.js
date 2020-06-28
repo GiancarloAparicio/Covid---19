61332136a9295b34d641ea3ed9f13ff0dd7005d7
Giancarlo Aparicio <zairo2542@gmail.com>
2020-06-28 14:25:56 -0500

    Updates

---
+++ precache-manifest.0e86a8b8816c854913e70620fe9a6308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c81d6e9f438beb48792f32f6d64889e",
+    "url": "/GiancarloAparicio/Covid---19/index.html"
+  },
+  {
+    "revision": "0c8952a5205ab06c2783",
+    "url": "/GiancarloAparicio/Covid---19/static/css/main.d2250f00.chunk.css"
+  },
+  {
+    "revision": "ead32ed926d6fc326582",
+    "url": "/GiancarloAparicio/Covid---19/static/js/2.748cfbc4.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/GiancarloAparicio/Covid---19/static/js/2.748cfbc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8952a5205ab06c2783",
+    "url": "/GiancarloAparicio/Covid---19/static/js/main.446f3b3d.chunk.js"
+  },
+  {
+    "revision": "139a07a92570c45d2607",
+    "url": "/GiancarloAparicio/Covid---19/static/js/runtime-main.dc6b0701.js"
+  }
+]);
